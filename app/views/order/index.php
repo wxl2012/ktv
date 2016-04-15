cf1b253c4e492ae8b00b6ae53b58844d7040f5ce
rayby2013 <zyr.wxl@gmail.com>
2016-04-15 16:13:52 +0800

    去除下拉刷新

--- app/views/order/index.php
+++ app/views/order/index.php
@@ -1,8 +1,3 @@
-<style>
-    #wrapper{
-        top: 50px !important;
-    }
-</style>
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="row">
@@ -18,64 +13,11 @@
     </div>
 </nav>
 
+<div style="height: 55px"></div>
 <div class="container">
-    <div id="wrapper">
-        <div id="scroller">
-
-            <div id="pull-down" class="text-center" style="display: none;">
-                <i class="fa fa-arrow-down"></i>
-                <span>下拉刷新数据...</span>
-            </div>
-
-           <div class="list-group clearfix">
-            </div>
-
-            <div id="pull-up" class="text-center">
-                <i class="fa fa-arrow-up"></i>
-                <span>上拉加载更多...</span>
-            </div>
-
-        </div>
+    <div class="list-group">
     </div>
 </div>
-
-<script type="text/x-jquery-tmpl" id="orderItem1">
-    <div class="list-group-item">
-        <div class="row">
-            <div class="col-xs-7" style="padding-left: 5px; padding-right: 0px;">
-                订单号: ${order_no}
-            </div>
-            <div class="col-xs-5 text-right" style="padding-left:0px; padding-right: 5px;">
-                ${created_at}
-            </div>
-        </div>
-        {{each(i, detail) details}}
-        <div class="list-group-item">
-            <div class="row">
-                <div class="col-xs-4" style="padding-left: 2px; padding-right: 0px;">
-                    <img src="${detail.goods.thumbnail}" alt="${detail.goods.title}" style="width: 100%; height: 100%;"/>
-                </div>
-                <div class="col-xs-8">
-                    <dl style="margin-top: 0px; margin-bottom: 0px;">
-                        <dt>${detail.goods.title}</dt>
-                        <dd>数量: ${detail.num} <span style="padding-left: 5px">单价: ${detail.price}</span></dd>
-                    </dl>
-                </div>
-            </div>
-        </div>
-        {{/each}}
-        <div class="row" style="margin-bottom: 10px; padding-top: 5px; padding-bottom: 5px;">
-            <div class="col-xs-3" style="padding-left: 5px; padding-right: 0px; line-height: 28px;">
-                <label class="label label-danger">待支付</label>
-            </div>
-            <div class="col-xs-9 text-right" style="padding-left:0px; padding-right: 5px;">
-                <a class="btn btn-sm btn-danger">关闭订单</a>
-                <a class="btn btn-sm btn-danger">删除订单</a>
-                <a class="btn btn-sm btn-warning">去支付</a>
-            </div>
-        </div>
-    </div>
-</script>
 <script type="text/x-jquery-tmpl" id="orderItem">
     <div class="list-group-item">
         <div class="row">
@@ -117,12 +59,6 @@
 </script>
 
 <?php
-
-/*$script = <<<js
-js;
-\Asset::js($script, [], 'before-script', true);*/
-
-\Asset::js(['jquery-tmpl/jquery.tmpl.min.js', 'jquery-tmpl/jquery.tmplPlus.min.js', 'iscroll/5.2.0/iscroll-infinite.js', 'tool.js', 'ucenter/order/index.js'], [], 'js-files', false);
-\Asset::css(['iscroll/5.2.0/iscroll.css'], [], 'css-files', false);
+\Asset::js(['jquery-tmpl/jquery.tmpl.min.js', 'jquery-tmpl/jquery.tmplPlus.min.js', 'tool.js', 'ucenter/order/index.js'], [], 'js-files', false);
 
 ?>
--- assets/app/ucenter/order/index.js
+++ assets/app/ucenter/order/index.js
@@ -1,118 +1,9 @@
-var scroll;
-var currentDirection;
-var flag = false;
-var currentPageIndex = 1;   //当前页面
-var lastId = 0;             //最后的一个ID
+
+var currentPageIndex = 1    ;
+
 $(function () {
-
-    scroll = new IScroll('#wrapper', {
-        probeType: 3,
-        mouseWheel: true
-    });
-
-    scroll.on('scroll', function () {
-        if(this.directionY == 1){
-            scrollUp();
-        }else if(this.directionY == -1){
-            scrollDown();
-        }
-    });
-    scroll.on('scrollEnd', function () {
-        var flag = currentDirection.attr('id') == 'pull-down';
-        var params = {'padding-top': '10px'};
-        if( ! flag){
-            params = {
-                'padding-bottom' : '10px'
-            };
-        }
-        currentDirection.animate(params, function () {
-
-            currentDirection.find('span').text('数据加载中...');
-            currentDirection.find('i').removeClass('fa-arrow-up').addClass('fa-spinner fa-spin');
-
-            if(flag){
-                loadNewData();
-            }else{
-                loadMoreData();
-            }
-
-        });
-    });
-
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-
     loadMoreData();
 });
-
-/**
- * 刷新数据
- */
-function scrollDown() {
-    currentDirection = $('#pull-down');
-    currentDirection.show();
-    var height = currentDirection.css('padding-top').replace('px', '');
-    height ++;
-    if(height > 20){
-        if(flag){
-            return;
-        }
-        flag = true;
-        currentDirection.find('i').removeClass('fa-arrow-down').addClass('fa-arrow-up');
-        currentDirection.find('span').text('松手开始刷新...');
-    }
-    currentDirection.css('padding-top', height + 'px');
-}
-
-/**
- * 更多数据
- */
-function scrollUp() {
-
-    currentDirection = $('#pull-up');
-    if(scroll.y < (scroll.maxScrollY - 55)){
-        currentDirection.show();
-        if(scroll.y < (scroll.maxScrollY - 30)){
-            if(flag){
-                return;
-            }
-            flag = true;
-            currentDirection.find('i').removeClass('fa-arrow-down').addClass('fa-arrow-up');
-            currentDirection.find('span').text('松手开始加载...');
-        }
-    }
-}
-
-/**
- * 加载新数据
- */
-function loadNewData() {
-    $.get('/api/order/list',
-        {
-            access_token: 'MGE3MTYyYjIzODYzNjY5NDRiYzE2NTUwM2U2ZGQ5ODI=',
-            last_id: lastId
-        },
-        function (data) {
-
-            flag = false;
-            $('#pull-down').hide();
-            $('#pull-down').find('i').removeClass('fa-spinner').removeClass('fa-spin').addClass('fa-arrow-down');
-            $('#pull-down').find('span').text('下拉刷新数据...');
-
-            if(data.status == 'err'){
-                return;
-            }
-
-            var items = data.data.items;
-            if(ObjectEmpty(items)){
-                return;
-            }
-            
-            for(var index in items){
-                $('.list-group').prepend(orderItem, items[index], null);
-            }
-            
-        }, 'json');
-}
 
 /**
  * 加载更多数据
@@ -125,10 +16,10 @@ function loadMoreData() {
         },
         function (data) {
 
-            flag = false;
+            /*flag = false;
             $('#pull-up').hide();
             $('#pull-up').find('i').removeClass('fa-spinner').removeClass('fa-spin').addClass('fa-arrow-up');
-            $('#pull-up').find('span').text('上拉加载更多...');
+            $('#pull-up').find('span').text('上拉加载更多...');*/
             if(data.status == 'err'){
                 return;
             }
